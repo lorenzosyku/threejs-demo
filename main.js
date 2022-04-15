c6127fb1b3636e41e6b4dfd57e709a5a3ea659be
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-04-14 20:16:06 -0400

    added orbit control

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -38,13 +39,18 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(lightHelper)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement)
 
 function animate() {
   requestAnimationFrame(animate);
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
   renderer.render(scene, camera);
 }
 
