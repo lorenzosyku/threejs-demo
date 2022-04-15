b2abfac828513f85cbf460cff7b62a4eb1ef236e
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-04-14 22:07:26 -0400

    added some content for the page

--- main.js
+++ main.js
@@ -59,8 +59,36 @@ function addStar(){
 Array(200).fill().forEach(addStar);
 
 const spaceTexture = new THREE.TextureLoader().load('nasa.jpg');
-scene.background = spaceTexture
+scene.background = spaceTexture;
 
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+function moveCamera(){
+  
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05 
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
 
 function animate() {
   requestAnimationFrame(animate);
